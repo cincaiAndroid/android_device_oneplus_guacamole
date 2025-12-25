5a62f24f0d355931b15c9790359d182785ed2968
cincaiAndroid <224403039+cincaiAndroid@users.noreply.github.com>
2025-12-25 15:02:32 +0800

    Add build configuration for OnePlus 7 Pro
    This file defines the build configuration for the OnePlus 7 Pro device, including product name, manufacturer, model, and build properties.

---
+++ cincai_guacamole.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
+$(call inherit-product, device/oneplus/guacamole/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := cincai_guacamole
+PRODUCT_DEVICE := guacamole
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := GM1911
+PRODUCT_BRAND := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="OnePlus7Pro-user 12 SKQ1.211113.001 P.202303230244 release-keys" \
+    BuildFingerprint=OnePlus/OnePlus7Pro/OnePlus7Pro:12/SKQ1.211113.001/P.202303230244:user/release-keys \
+    DeviceName=OnePlus7Pro \
+    DeviceProduct=OnePlus7Pro \
+    SystemDevice=OnePlus7Pro \
+    SystemName=OnePlus7Pro
